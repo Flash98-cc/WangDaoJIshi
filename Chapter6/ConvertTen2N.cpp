a0c741babba487f0214c54e1332033ad3ddab1c6
Barry <53329692+Flash98-cc@users.noreply.github.com>
2022-03-11 21:09:23 +0800

    Update ConvertTen2N.cpp

--- Chapter6/ConvertTen2N.cpp
+++ Chapter6/ConvertTen2N.cpp
@@ -1,20 +1,28 @@
- //十进制转换成任意进制
+//十进制转换成任意进制
 #include <iostream>
 #include <vector>
 using namespace std;
 
+char IntTChar(int current)//数字转字符
+{
+    if(current<10)
+    {
+        return current+'0';
+    }
+    else return current-10+'A';//十六进制换成A-F
+}
 
 void ConvertTen2N(int number,int n)
 {
-    vector<int> result;
+    vector<char> result;
     if(number==0)
     {
-        result.push_back(0);
+        result.push_back('0');
     }
     else{//转换
          while(number!=0)
         {
-            result.push_back(number%n);
+            result.push_back(IntTChar(number%n));
             number=number/n;
         }
     }
@@ -30,7 +38,7 @@ int main()
     int number;
     while(cin>>number)
     {
-        ConvertTen2N(number,8);
+        ConvertTen2N(number,16);
     }
     return 0;
 }
